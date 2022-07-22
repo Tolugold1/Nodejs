866b29bbacedc1d68e6a0ad27219e006fa8f43d7
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-07-22 17:24:57 +0100

    first server example

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-var rect = require('./rectangle');
+const http = require('http');
+const port = 3000;
+const hostname = "localhost";
 
-function Node(l, b) {
-   console.log("Solving for area and perimeter of a rectangle with l " + l + " and b "+ b);
-   rect(l, b, (err, rectangle) => {
-      if (err) {
-         console.log("Error:", err.message)
-      } else {
-         console.log("The perimeter of the rectangle with l " + l + " and b " + b + " is " + rectangle.perimeter());
-         console.log("The area of the rectangle with l " + l + " and b " + b + " is " + rectangle.area())
-      }
-   });
-   console.log("This statement is called after the call to rect()");
-}
+const server = http.createServer((req, res) => {
+   console.log(req.headers)
+   res.statusCode = 200;
+   res.setHeader("Content-Type", "text/html");
+   res.end('Hello Tolu!!!');
+})
 
-Node(2,4);
-Node(3,5);
-Node(0,5);
-Node(-3,5);
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`)
+})

8f37ebabba4fbad36fa0154fcf07f3701fd46c9c
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-07-23 14:34:11 +0100

    update

--- node-express/index.js
+++ node-express/index.js
@@ -3,52 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const hostname = 'localhost';
-const fs = require('fs');
-const path = require('path');
+const morgan = require('morgan')
 
+
+app.use(express.static(__dirname + "/public-express"));
+
+app.use(morgan('dev'));
 app.use((req, res) => {
    console.log("Request for " + req.url + " with method " + req.method);
    
-   if (req.method === 'GET') {
-      var fileUrl;
-
-      if (req.url === '/') {
-         fileUrl = '/index.html';
-      } else {
-         fileUrl = req.url;
-      }
-
-      var filePath = path.resolve('./public-express' + fileUrl);
-      const fileExtension = path.extname(filePath);
-      if (fileExtension === '.html') {
-         fs.exists(filePath, (exist) => {
-            if (!exist) {
-               res.statusCode = 404;
-               res.setHeader("Content-Type", "text/html");
-               res.end("Error 404: " + filePath + " not found");
-
-               return;
-            } else {
-               res.statusCode = 200;
-               res.setHeader("Content-Type", "text/html");
-               fs.createReadStream(filePath).pipe(res);
-            }
-         });
-      } else {
-         res.statusCode = 404;
-         res.setHeader("Content-Type", "text/html");
-         res.end("Error 404: sorry!!! " + filePath + " is not an HTML file.")
-
-         return;
-      }
-
-   } else {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html")
-      res.end("This request: " + req.method + " method is not supported");
-
-      return;
-   }
+   res.statusCode = 200;
+   res.setHeader("Content-Type", "text/html");
+   res.end("This is an express server page")
 });
 
 const server = http.createServer(app);
@@ -56,3 +22,5 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
    console.log(`Server running at http://${hostname}:${port}`);
 })
+
+
